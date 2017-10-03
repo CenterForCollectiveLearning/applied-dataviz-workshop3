1ace8456c475944e1e3f6b5ef9ae030d9a81b933
Jingxian Zhang <jingxian@Jingxians-MacBook-Pro.local>
2017-10-03 13:29:34 -0400

    adjustments in maps and bubble chart

--- bubble.js
+++ bubble.js
@@ -5,10 +5,10 @@ var color = d3.scale.category10()
  .range( ['#2AA4A9', '#57B28D','#FBAE4B', '#F16045', '#D12258', '#5E4E73', '#C2B49B', '#734743', '#80A464', '#435773']);
 var bubble = d3.layout.pack()
     .sort(null)
-    .size([diameter * 1.4, diameter])
-    .padding(1.5);
+    .size([diameter * 1.4, diameter]) //size of the bubble chart
+    .padding(1);
 var svg = d3.select("body").select("#svg_bubble");
-var tooltip = d3.select("body")
+var tooltip = d3.select("body") //set the tooltip
     .append("div")
     .style("position", "absolute")
     .style("z-index", "10")
@@ -20,11 +20,13 @@ var tooltip = d3.select("body")
     .style("font", "12px PT Sans")
     .text("tooltip");
 var habbits = [
-    {category: "sports", name: "running", value: 0.3},
-    {category: "sports", name: "swimming", value: 0.1},
-    {category: "sports", name: "badminton", value: 0.1},
-    {category: "cooking", name: "cooking", value: 0.3},
-    {category: "music", name: "piano", value: 0.3}
+    {category: "sports", name: "running", alt: "helps me think:)", value: 0.3},
+    {category: "sports", name: "swimming", alt: "I enjoy the quietness in the water", value: 0.1},
+    {category: "sports", name: "badminton", alt: "sweating and relaxing", value: 0.05},
+    {category: "cooking", name: "cooking", alt: "and eating!", value: 0.2},
+    {category: "enjoy", name: "piano", alt: "♪♫♬", value: 0.15},
+    {category: "enjoy", name: "doodling", alt: "#$#%*&^$#@", value: 0.1},
+    {category: "enjoy", name: "shows", alt: "watch with friends:D", value: 0.1}
 ];
 var node = svg.selectAll(".node")
     .data(bubble.nodes({children:habbits}).filter(function(d) { return !d.children; }))
@@ -32,18 +34,26 @@ var node = svg.selectAll(".node")
     .attr("class", "node")
     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 node.append("circle")
-    .attr("r", function(d) { console.log(d);return d.r; })
+    .attr("r", function(d) { return d.r; })
     .style("fill", function(d) { return color(d.category); })
     .on("mouseover", function(d) {
-            tooltip.text(d.name + ": " + (d.value*100) + "%");
+            //d3.select(this.parentNode).select("text").text(d.alt);
+            d3.select(this).style("fill", function(d) { return "rgba(220,220,220,0.8)"; });
+            //tooltip.text(d.name + " takes up about " + (d.value*100) + "% of my spare time");
+            tooltip.text(d.alt);
             tooltip.style("visibility", "visible");
     })
     .on("mousemove", function() {
         return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
     })
-    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+    .on("mouseout", function(){
+        //d3.select(this.parentNode).select("text").text(function(d) { return d.name; });
+        d3.select(this).style("fill", function(d) { return color(d.category); });
+        return tooltip.style("visibility", "hidden");
+    });
 node.append("text")
     .attr("dy", ".3em")
     .style("text-anchor", "middle")
     .style("pointer-events", "none")
-    .text(function(d) { return d.name.substring(0, d.r / 3); });
+    .style("font-size", function(d){ return 18/60*d.r + "px"}) //adjust font-size based on node radius
+    .text(function(d) { return d.name; });
--- map.js
+++ map.js
@@ -1,116 +1,28 @@
-/*//map using d3+ (dots)
-const points = [
-    {id: "Boston", value: 1834, point: [-71.0589, 42.3601], country: "usa"},
-    {id: "Champaign", value: 3360, point: [-88.243383, 40.116420], country: "usa"},
-    {id: "Hefei", value: 2602, point: [117.227219, 31.820591], country: "china"},
-    {id: "Tianjin", value: 4302, point: [117.361648, 39.343357], country: "china"}
-];
-
-let geomap = new d3plus.Geomap()
-    .select("#svg_map")
-    .data(points)
-    .ocean("#eef")
-    .tiles(true)
-    .pointSizeMin(10)
-    .render();*/
-
-/*//map using d3+ (color the countries)
+//map using d3+ (color the countries)
 var sample_data = [
-    {"value": 1992, "country": "aschn", "name": "China"},
-    {"value": 2014, "country": "eudeu", "name": "Germany"},
-    {"value": 2014, "country": "nausa", "name": "United States"},
-    {"value": 2016, "country": "nabhs", "name": "Bahamas"},
-    {"value": 2017, "country": "sachl", "name": "Chile"}
-]
+    {"year": "1992 - 2014", "country": "aschn", "name": "China", "tags": "birth country; undergraduate", "color": "#ABDDA4"},
+    {"year": "2014", "country": "eudeu", "name": "Germany", "tags": "conference", "color": "#ABDDA4"},
+    {"year": "2014 - 2017", "country": "nausa", "name": "United States", "tags": "graduate", "color": "#ABDDA4"},
+    {"year": "2016", "country": "nabhs", "name": "Bahamas", "tags": "travel", "color": "#ABDDA4"},
+    {"year": "2017", "country": "sachl", "name": "Chile", "tags": "research", "color": "#ABDDA4"}
+];
 
 var visualization = d3plus.viz()
     .container("#svg_map")        // container DIV to hold the visualization
-    .data(sample_data)        // data to use with the visualization
-    .coords("http://d3plus.org/topojson/countries.json") // pass topojson coordinates
-    .type("geo_map")          // visualization type
-    .id("country")            // key for which our data is unique on
-    .text("name")             // key to use for display text
-    .color("value")           // key for coloring countries
-    .tooltip("value")         // keys to place in tooltip
-    .draw()                   // finally, draw the visualization!
-*/
+    .data(sample_data)            // data to use with the visualization
+    .coords({
+        "mute": ["anata"],
+        "value": "http://d3plus.org/topojson/countries.json"
+    }) // pass topojson coordinates
+    .type("geo_map")              // visualization type
+    .id("country")                // key for which our data is unique on
+    .text("name")                 // key to use for display text
+    .color("color")               // key for coloring countries
+    .tooltip(["year", "tags"])              // keys to place in tooltip
+    .draw();                      // finally, draw the visualization!
 
-/*//map using d3
-var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-              return "<strong>Country: </strong><span class='details'>" + d.name + "<br></span>" + "<strong>Time: </strong><span class='details'>" + d.year +"</span>";
-            })
 
-var margin = {top: 20, right: 20, bottom: 30, left: 30},
-            width = 1200 - margin.left - margin.right,
-            height = 800 - margin.top - margin.bottom;
-
-var color = d3.scale.threshold()
-    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
-
-var path = d3.geo.path();
-
-var svg = d3.select("body")
-            .select("#svg_map")
-            .append('g')
-            .attr('class', 'map');
-
-var projection = d3.geo.mercator()
-                   .scale(130)
-                  .translate( [width / 2, height / 1.5]);
-
-var path = d3.geo.path().projection(projection);
-
-svg.call(tip);
-
-var my_data = [
-    {"year": "1992 - 2014", "id": "CHN", "name": "China"},
-    {"year": "2014", "id": "DEU", "name": "Germany"},
-    {"year": "2014 - 2017", "id": "USA", "name": "United States"},
-    {"year": "2016", "id": "BHS", "name": "Bahamas"},
-    {"year": "2017", "id": "CHL", "name": "Chile"}
-]
-  
-svg.append("g")
-    .attr("class", "countries")
-  .selectAll("path")
-    .data(my_data)
-  .enter().append("path")
-    .attr("d", path)
-    .style("fill", function(d) { return color(0); })
-    .style('stroke', 'white')
-    .style('stroke-width', 1.5)
-    .style("opacity",0.8)
-    // tooltips
-      .style("stroke","white")
-      .style('stroke-width', 0.3)
-      .on('mouseover',function(d){
-        tip.show(d);
-
-        d3.select(this)
-          .style("opacity", 1)
-          .style("stroke","white")
-          .style("stroke-width",3);
-      })
-      .on('mouseout', function(d){
-        tip.hide(d);
-
-        d3.select(this)
-          .style("opacity", 0.8)
-          .style("stroke","white")
-          .style("stroke-width",0.3);
-      });
-
-svg.append("path")
-    .datum(topojson.mesh(my_data, function(a, b) { return a.id !== b.id; }))
-    .attr("class", "names")
-    .attr("d", path);
-*/
-
-//datamaps
+/*//using datamaps (d3) http://datamaps.github.io
 var basic_choropleth = new Datamap({
   element: document.getElementById("svg_map"),
   projection: 'mercator',
@@ -133,4 +45,4 @@ var basic_choropleth = new Datamap({
                 '</strong></div>'].join('');
     }
   }
-});
+});*/
